66137c8cb9435a1e03c1cb4f61127eea362fb29a
gumiranda <imartytk@hotmail.com>
2020-07-29 19:05:37 -0300

    manifest

--- mobile/index.js
+++ mobile/index.js
@@ -2,10 +2,10 @@
  * @format
  */
 import 'expo-asset';
-import { AppRegistry } from 'react-native';
+import {AppRegistry} from 'react-native';
 import {registerRootComponent} from 'expo';
 import App from './App';
-import { name as appName } from './app.json';
+import {name as appName} from './app.json';
 
 //AppRegistry.registerComponent(appName, () => App);
 registerRootComponent(App);
